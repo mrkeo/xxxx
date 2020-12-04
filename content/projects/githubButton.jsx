b3aafcb1a4695ba622e65e5af16b37177476bb6a
Paweł Idzikowski <iddziku@gmail.com>
2020-12-04 14:20:08 +0100

    use redirectButton as button that only references GitHub repo

--- content/projects/githubButton.jsx
+++ content/projects/githubButton.jsx
@@ -1,33 +1,34 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import "../../src/styles/global.css";
 
-class RedirectButton extends Component {
+class GitHubButton extends Component {
   openUrl(url) {
     window.open(url, "_blank");
   }
 
   render() {
-    let text;
     let url;
     if (this.props) {
-      ({ text, url } = this.props);
+      ({ url } = this.props);
     }
 
     return (
       <div>
         <Button
+          className="w-100"
           variant="contained"
           color="primary"
           onClick={() => this.openUrl(url)}
           size="large"
           startIcon={<GitHubIcon />}
         >
-          {text}
+          Find out more at GitHub
         </Button>
       </div>
     );
   }
 }
 
-export default RedirectButton;
+export default GitHubButton;
