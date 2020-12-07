5099fc5c63ee910408d40fdeec328b9e19196c02
Paweł Idzikowski <iddziku@gmail.com>
2020-12-07 11:15:07 +0100

    Update footer.tsx

--- src/@lekoarts/gatsby-theme-emma/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-emma/components/footer.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { Box, Link, Flex, jsx } from "theme-ui"
+import { Box, Link, Flex, jsx } from "theme-ui";
 
 const Footer = () => (
   <Box as="footer" variant="layout.footer">
@@ -9,28 +9,73 @@ const Footer = () => (
       sx={{
         justifyContent: `center`,
         alignItems: `center`,
-        mt: 3,
+        height: "30px",
+        mt: 2,
+        mb: 0,
         color: `text`,
         fontWeight: `semibold`,
         a: { color: `text` },
       }}
     >
-      <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_w30.png" alt="LekoArts Logo" />
+      <div sx={{ position: "relative", top: "40%" }}>
+        <img
+          width="25"
+          height="25"
+          src="https://img.lekoarts.de/gatsby/logo_w30.png"
+          alt="LekoArts Logo"
+        />
+      </div>
       {` `}
       <Link
         aria-label="Link to the theme's GitHub repository"
         sx={{ ml: 2 }}
+        target="_blank"
         href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-emma"
       >
         Theme
       </Link>
       <div sx={{ mx: 1 }}>by</div>
       {` `}
-      <Link aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
+      <Link
+        aria-label="Link to the theme author's website"
+        target="_blank"
+        href="https://www.lekoarts.de/en"
+      >
         LekoArts
       </Link>
     </Flex>
+    <Flex
+      sx={{
+        justifyContent: `center`,
+        alignItems: `center`,
+        height: "30px",
+        margin: 0,
+        color: `text`,
+        fontWeight: `semibold`,
+        a: { color: `text` },
+      }}
+    >
+      <div sx={{ mx: 1 }}>modified by</div>
+      {` `}
+      <Link
+        sx={{ mr: 2 }}
+        aria-label="Link to the modified theme author's GitHub"
+        target="_blank"
+        href="https://github.com/trolit"
+      >
+        trolit
+      </Link>
+      {` `}
+      <div sx={{ position: "relative", top: "40%" }}>
+        <img
+          width="25"
+          height="25"
+          src="avatar.png"
+          alt="trolit GitHub avatar"
+        />
+      </div>
+    </Flex>
   </Box>
-)
+);
 
-export default Footer
+export default Footer;
