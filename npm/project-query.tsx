7fa724266fa99d0f029cb48ceb374cb9230c28d6
Paweł Idzikowski <iddziku@gmail.com>
2020-12-14 16:42:40 +0100

    Add files

---
+++ npm/project-query.tsx
@@ -0,0 +1,30 @@
+import { graphql } from "gatsby"
+import ProjectComponent from "../components/project"
+
+export default ProjectComponent
+
+export const query = graphql`
+  query($slug: String!, $formatString: String!) {
+    project(slug: { eq: $slug }) {
+      body
+      excerpt
+      client
+      color
+      date(formatString: $formatString)
+      labels
+      service
+      slug
+      title
+      cover {
+        childImageSharp {
+          fluid(maxWidth: 1920, quality: 90) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+          resize(width: 800) {
+            src
+          }
+        }
+      }
+    }
+  }
+`
