9e2f0fd4d0e8835d2c275a75e357a1b0e202542c
Paweł Idzikowski <iddziku@gmail.com>
2020-12-16 13:19:39 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f434dd8737c11da5ccc4.js"
+    "url": "webpack-runtime-27b0a402d61accfec2e6.js"
   },
   {
     "url": "styles.161eb81eb2c1183a0532.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5ada85f1419ec65e620d347fd1aa3de2"
+    "revision": "6da206eaec296a74fcd5b267423ef418"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "31f9bb716e8024aae3d9b725e147efcb"
+    "revision": "900fbabb364a13a87157cb9a94adbe33"
   },
   {
     "url": "polyfill-78cdc6a84a6e659c51c0.js"
